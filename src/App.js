88049087dbb9698a51b9e2e5a7a9dc90b73f13e3
banuaggun <banuaggun@outlook.com>
2022-10-27 15:18:46 +0300

    Post adding form created & post adding event done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import AddPostForm from './features/posts/AddPostForm';
 import PostsList from './features/posts/PostsList';
 
 function App() {
   return (
     <main className="App">
+      <AddPostForm/>
       <PostsList/>
+      
     </main>
   );
 }
