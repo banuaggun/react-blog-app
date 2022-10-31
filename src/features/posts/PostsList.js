dc915628b4c8eae6346f3b8bcc2a12e10a1fb1ff
banuaggun <banuaggun@outlook.com>
2022-10-31 16:45:31 +0300

    Added Timestamp for Posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import PostAuthor from "./PostAuthor";
 
 import { selectAllPosts } from "./postsSlice";
+import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
 
@@ -13,6 +14,7 @@ const PostsList = () => {
       <p>{post.content.substring(0, 100)}</p>
       <p className="postCredit">
         <PostAuthor userId = {post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ))
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,15 +1,19 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
+import {sub} from "date-fns";
+
 const initialState = [
   {
     id: "1", 
     title: "React Blog App Project",
-    content: "I am preparing this project to take my react js skills to the next level and learn redux."
+    content: "I am preparing this project to take my react js skills to the next level and learn redux.",
+    date: sub(new Date(), {minutes:10}).toISOString()
   },
   {
     id: "2",
     title: "Design Principles",
-    content: "Design principles and the latest developments in design"
+    content: "Design principles and the latest developments in design",
+    date: sub(new Date(), {minutes:5}).toISOString()
   }
 ]
 
@@ -27,6 +31,7 @@ const postsSlice = createSlice({
             id:nanoid(),
             title,
             content,
+            date:new Date().toISOString(),
             userId
           }
         }
