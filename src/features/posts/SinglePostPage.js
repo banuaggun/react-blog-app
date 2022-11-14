0b85844c5717591e234ef908b1f89dc714675f70
banuaggun <banuaggun@outlook.com>
2022-11-14 13:27:57 +0300

    Edit Post Routing Problem Solved

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -27,7 +27,7 @@ const SinglePostPage = () => {
       <h2>{post.title}</h2>
       <p>{post.body}</p>
       <p className='postCredit'>
-        <Link to={`/post/${post.id}`}>Edit Post</Link>
+        <Link to={`/post/edit/${post.id}`}>Edit Post</Link>
         <PostAuthor userId = {post.userId} />
         <TimeAgo timestamp={post.date} />
       </p>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -105,7 +105,7 @@ const postsSlice = createSlice({
         action.payload.date = new Date().toISOString();
         action.payload.reactions = {
           thumbsUp: 0,
-          hooray: 0,
+          wow: 0,
           heart: 0,
           rocket: 0,
           coffee: 0
