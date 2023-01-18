0979c7a86ba4bffc3f7f73a34c03a2950147d2b1
banuaggun <banuaggun@outlook.com>
2023-01-18 22:11:40 +0300

    About page made completely responsive

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import '../assets/styles/about.css'
 
+import feather from "../assets/icons/feather2.svg"
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about__page'>
+      <div className='about__page__title'>
+        <span>About The Project</span>
+      </div>
+      <div className='about__page__body'>
+        <p>
+        In this project, I created a blog page that I designed using Figma and coded it using ReactJs and Redux frameworks. I got the blog data from a remote source using API. I did the decoding of the design with PureCSS.
+        </p>
+      </div>
+      <div className='about__page__end'>
+        <p>You can reach my other projects from the links below</p>
+        <div className='about__page__end__links'>
+        <a href="https://portfolio-banuaggun.vercel.app/" rel="noreferrer noopenner" target="_blank">
+          <span className='icon icon-fill'>
+            <i className=''></i>
+          </span>  
+          </a>
+          <a href="https://www.linkedin.com/in/banuaggun/?locale=en_US" rel="noreferrer noopenner" target="_blank">
+            <span className='icon icon-fill'>
+            <i className="bi bi-linkedin"></i>
+            </span>
+            
+          </a>
+          <a href="https://www.behance.net/banuaggun" rel="noreferrer noopenner" target="_blank">
+            <span className='icon icon-fill'>
+            <i className="bi bi-behance"></i>
+            </span>
+            
+          </a>
+          <a href="https://dribbble.com/banuaggun" rel="noreferrer noopenner" target="_blank">
+            <span className='icon icon-fill'>
+            <i className="bi bi-dribbble"></i>
+            </span>
+            
+          </a>
+          
+         
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
